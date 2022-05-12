11cbad6302a64acf912868d74c4736993be0eaae
his_username <hemantnagar28@yahoo.com>
2022-05-12 15:47:12 +0530

    java_revise_ultimate_project

---
+++ src/revision_12_5_22/DemoVariables.java
@@ -0,0 +1,41 @@
+package revision_12_5_22;
+
+public class DemoVariables {
+
+    public static void main(String[] args) {
+
+        String city="Agra";
+        System.out.println("You live in "+city);
+
+        int age=24;
+        System.out.println("You are " + age + " years of age");
+
+        float salary=95635.52f;
+        System.out.println("your salary si "+salary);
+        
+        String name="Rohan";
+        int marks=89;
+        System.out.println(name +" ,you got " +marks+ " marks in Maths");
+
+        String name1="Ashok";
+        int age1=23;
+        float salary1=165365.500f;
+
+        System.out.println(name1+ " at the age of "+age1+ " you got this much of salary "+salary1);
+
+        String name2="Harry";
+        String subject1="History";
+        String subject2="English";
+        String subject3="Hindi";
+        String subject4="Maths";
+        String subject5="Science";
+
+        int marksH=89;
+        int marksE=75;
+        int marksHi=78;
+        int marksM=84;
+        int marksS=94;
+
+        System.out.println(name2+ " your marks in "+subject1+","+ subject2+"," + subject3+"," + subject4+"," + subject5+"," + " are "+ marksH+"," + marksE+"," + marksHi+"," + marksM+"," + marksS+"," + " respectively");
+    }
+}
---
+++ src/revision_12_5_22/Revision.java
@@ -0,0 +1,15 @@
+package revision_12_5_22;
+
+public class Revision {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+
+        System.out.println("I have complete command over java programming language");
+
+        System.out.println("How do we know each other?");
+
+        System.out.println("I have taken command of java.");
+    }
+}
